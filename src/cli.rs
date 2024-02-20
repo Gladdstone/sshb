51e4748867620dbaa689ca76a22f2dadd90e9d0e
Joseph Farrell <disastrousgamemaster@gmail.com>
2024-02-19 18:57:56 -0800

    add list, automatic config updates

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,8 @@
+use std::io::{self, Write};
+
 use clap::{command, Arg, ArgAction, Command};
+
+use crate::config_manager::ConfigManager;
 
 
 pub fn builder() -> Command {
@@ -9,6 +13,10 @@ pub fn builder() -> Command {
         .long("ssh")
         .action(ArgAction::Set)
         .help("help text"),
+      Arg::new("list")
+        .short('l')
+        .long("list")
+        .action(ArgAction::SetTrue),
       Arg::new("add_config")
         .short('a')
         .long("add-config")
@@ -18,4 +26,43 @@ pub fn builder() -> Command {
         .long("remove-config")
         .action(ArgAction::Append)
     ])
+}
+
+pub fn prompt(message: &str, error_message: &str) -> Result<String, Box<dyn std::error::Error>> {
+  let mut user_input = String::new();
+
+
+  print!("{}", message);
+  let _ = std::io::stdout().flush();
+  io::stdin().read_line(&mut user_input)
+    .expect(&format!("{}", error_message));
+
+  return Ok(user_input);
+}
+
+pub fn parse_yn(value: &str) -> bool {
+  let lower_value = value.to_lowercase();
+
+  match lower_value.as_str() {
+    "yes" => return true,
+    "y" => return true,
+    "no" => return false,
+    "n" => return false,
+    _ => return false
+  };
+}
+
+pub fn add_config(config_manager: &ConfigManager, host: String) {
+  let mut hostname = String::new();
+  let mut user = String::new();
+  let mut password = String::new();
+
+  let input_error = "Error: Failed to read input";
+
+  hostname = prompt("Enter hostname: ", input_error).unwrap();
+  user = prompt("Enter user: ", input_error).unwrap();
+  password = prompt("Enter password: ", input_error).unwrap();
+
+  config_manager.add_config(&host, hostname.as_str(), user.as_str())
+    .expect(format!("Error: Failed to add config {host}").as_str());
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io::{self, Write};
-use clap::{Parser, value_parser};
+use std::env;
+use clap::Parser;
 
 mod cli;
 mod config_manager;
@@ -21,41 +21,38 @@ struct Cli {
 fn main() {
   println!("RUN: sshb");
 
-  // let matches = cli::builder().get_matches();
-  // let values = Value::from_matches(&matches);
+  let sshb_config = env::var("SSHB_CONFIG")
+    .expect("SSHB_CONFIG UNDEFINED");
 
-  let args = Cli::parse();
+  let matches = cli::builder().get_matches();
+  let config_manager = config_manager::ConfigManager { ssh_config: sshb_config };
 
-  if let Some(host) = args.ssh.as_deref() {
+  let input_error = "Error: Failed to read input";
+
+  if let Some(host) = matches.get_one::<String>("ssh") {
     println!("CLI COMMAND: ssh {}", host);
+    if !config_manager.existing_host(host).unwrap() {
+      let message = &format!("Add new host {}?", host.as_str());
+      let response = cli::prompt(message, input_error).unwrap();
+
+      if cli::parse_yn(response.as_str()) {
+        cli::add_config(&config_manager, host.to_string());
+      }
+    }
     secureshell::ssh(host, None, None);
   }
-  if let Some(add_config) = args.add_config.as_deref() {
-    let host = add_config;
-    let mut hostname = String::new();
-    let mut user = String::new();
-    let mut password = String::new();
+  if let Some(_) = matches.get_one::<bool>("list") {
+    let hostnames = config_manager.get_hostnames().expect("failed");
 
-    print!("Enter hostname: ");
-    let _ = std::io::stdout().flush();
-    io::stdin().read_line(&mut hostname)
-      .expect("Error: Failed to read input");
-
-    print!("Enter user: ");
-    let _ = std::io::stdout().flush();
-    io::stdin().read_line(&mut user)
-      .expect("Error: Failed to read input");
-
-    print!("Enter password: ");
-    let _ = std::io::stdout().flush();
-    io::stdin().read_line(&mut password)
-      .expect("Error: Failed to read input");
-
-    config_manager::add_config(host, hostname.as_str(), user.as_str())
-      .expect(format!("Error: Failed to add config {host}").as_str());
+    for host in hostnames {
+      print!("{}\n", host);
+    }
   }
-  if let Some(host) = args.remove_config.as_deref() {
-    let _ = config_manager::remove_config(host)
+  if let Some(add_config) = matches.get_one::<String>("add_config") {
+    cli::add_config(&config_manager, add_config.to_string());
+  }
+  if let Some(host) = matches.get_one::<String>("remove_config") {
+    let _ = config_manager.remove_config(host)
       .expect(format!("Error: Failed to remove config {host}").as_str());
   }
 }
